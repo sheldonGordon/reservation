c0b448dcc1a65835a365ac532687a857f468d15e
aurélien <achatelain62000@gmail.com>
2020-10-26 21:59:39 +0100

    Remove spring boot
    trop chiant, la secu on verra plus tard

--- src/main/java/fr/chatelain/reservation/reservation/ReservationApplication.java
+++ src/main/java/fr/chatelain/reservation/reservation/ReservationApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@SpringBootApplication
 public class ReservationApplication {
 
 	public static void main(String[] args) {
